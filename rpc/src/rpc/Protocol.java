2ee54a364cdd3176aa2646e4a20897f8bd393cf7
burhanxz <930051163@qq.com>
2019-01-18 20:47:03 +0800

    新增doc图片

--- rpc/src/rpc/Protocol.java
+++ rpc/src/rpc/Protocol.java
@@ -9,6 +9,10 @@ public interface Protocol {
 	public <T> Exporter<T> export(Invoker<T> invoker);
 	/**
 	 * 引入远程服务
+	 * 
+     * 1. 当用户调用refer()所返回的Invoker对象的invoke()方法时，协议需相应执行同URL远端export()传入的Invoker对象的invoke()方法。
+     * 2. refer()返回的Invoker由协议实现，协议通常需要在此Invoker中发送远程请求。
+     * 3. 当url中有设置check=false时，连接失败不能抛出异常，需内部自动恢复。
 	 * @param type 服务的类型
 	 * @param url 远程服务的URL地址
 	 * @return 服务的本地代理
--- rpc/src/rpc/URL.java
+++ rpc/src/rpc/URL.java
@@ -1,5 +1,25 @@
 package rpc;
 
 public class URL {
-
+	private String host;
+	private int port;
+	private String path;
+	public String getHost() {
+		return host;
+	}
+	public void setHost(String host) {
+		this.host = host;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
 }
